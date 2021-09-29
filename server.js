110d9e4cc9ea43feca864940c3265667c3037f77
murshidazher <murshidazher@yahoo.com>
2021-09-30 01:21:13 +0530

    :sparkles: feat: version bump

--- server.js
+++ server.js
@@ -15,5 +15,14 @@ app.use((req, res, next) => {
 app.use("/", require("./routes/poll.js"));
 
 app.listen(8090, function() {
-    console.log("Listening on port 8090");
+  console.log("Listening on port 8090");
+  // Here we send the ready signal to PM2
+  process.send('ready');
 });
+
+process.on('SIGINT', function () {
+  // clear everything needed (like database connections, processing jobs…) before letting your application exit for a graceful shutdown
+   db.stop(function(err) {
+     process.exit(err ? 1 : 0)
+   })
+})
