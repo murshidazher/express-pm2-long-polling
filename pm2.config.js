e14bf3bb5495ad402b5a235638e0aeede11292d3
murshidazher <murshidazher@yahoo.com>
2021-09-30 02:56:20 +0530

    :sparkles: feat: version bump

--- pm2.config.js
+++ pm2.config.js
@@ -1,18 +1,12 @@
 module.exports = {
   name: "express-pm2-long-polling",
   script: "server.js",
-  instances: "3",
+  instances: "max",
   watch: false,
   autorestart: true,
   wait_ready: true,
-  ignore_watch : ["node_modules"],
   exec_mode: "cluster",
   instance_var: "APP_INSTANCE_SEQ",
   listen_timeout: 10000,
   restart_delay: 10000,
-  cwd: ".",
-  env: {
-    PM2_SERVE_PATH: '.',
-    PM2_SERVE_PORT: 3000,
-  }
 }
