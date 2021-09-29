f6aa313d57fbf309e28cd90d37be0a54f4c16d2f
murshidazher <murshidazher@yahoo.com>
2021-09-29 22:51:52 +0530

    :sparkles: feat: add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "express-pm2-long-polling",
+  script: "server.js",
+  instances: "2",
+  watch: true,
+  ignore_watch : ["node_modules"],
+  exec_mode: "cluster",
+  instance_var: "APP_INSTANCE_SEQ",
+  listen_timeout: 10000,
+  restart_delay: 10000,
+  cwd: ".",
+  env: {
+    PM2_SERVE_PATH: '.',
+    PM2_SERVE_PORT: 8090,
+  }
+}
